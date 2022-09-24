44fe0483ae219c3760008cec71373e140417f46d
AlexKamm <alexandkamm@comcast.net>
2022-09-23 21:33:13 -0400

    adding basic setup of computations file

---
+++ computations.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+# data based off most recent completed year
+
+# quick ratio
+# (cash and cash equivalances + account recievable)/ current liabilities
+def quick_ratio(cash: double, current_liabilities: double) -> double:
+
+# current ratio
+# current assests / current liabilites
+def current_ratio(current_assets: double, current_liabilities: double) -> double:
+
+# debt to equity ratio
+# total debt / #shareholders equity
+def debt_to_equity_ratio(total_debt: double, shareholder_equity: dobule) -> double:
+
+# price to earnings ratio
+# current stock price/ EPS from income statements
+def price_to_earnings_ratio(current_price: double, eps: double) -> double:
+
+# ROE (return on equity)
+# net income from income statement / shareholder equity
+def return_on_equity(net_income: double, shareholder_equity: double) -> double:
